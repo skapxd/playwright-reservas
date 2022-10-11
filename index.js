18f1adcdc304d30e65cb37ab68c1b2ede3ee4685
John Rivera <jrivera@q10.com>
2022-10-11 10:37:40 -0500

    ads

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // @ts-check
 // Documentacion del paquete -> https://playwright.dev/docs/api/class-playwright
 // Video de ayuda para playWright -> https://www.youtube.com/watch?v=YjbRkt8cew8
+
 const {chromium} = require('playwright');
 
 (async function main(){
   const browser = await   chromium.launch({headless: false})
   const page = await browser.newPage();
   await page.goto('https://jack.q10.com/login?ReturnUrl=%2F')
-  const inputName = await page.locator('[id="NombreUsuario"]').type('hola')
+  const inputName = await page.locator('[id="NombreUsuario"]').type('jrivera@q10.com')
+
+  
 })()
