10aa37f3f5d73fa8d646c7c5cf1bac63ee3ff7b2
mLondono <mlondono@q10.com>
2022-10-14 14:58:49 -0500

    abrir modal del ultimo dia

--- src/reservarDia.js
+++ src/reservarDia.js
@@ -3,15 +3,19 @@
  * @returns 
  */
 module.exports.reservarDia = async function(page){    
-  await page.locator('.nav-item').last().click();
-  await page.locator('[class="btn btn-primary q10button modal-form"]').click();
+  const lastDay = page.locator('.nav-item').last()
+  await lastDay.click();
 
-  await page.waitForSelector('#Reserva_res_compra_almuerzo', {state: "visible"});
+  const dataId = await lastDay.getAttribute('data-id-fecha')
 
-  await page.locator('#Reserva_res_compra_almuerzo').first().click()
+  await page.locator(`[href="/Q10/Asistencia/Crear/${dataId}/01"]`).click();
+
+  // await page.waitForSelector('#Reserva_res_compra_almuerzo', {state: "visible"});
+
+  // await page.locator('#Reserva_res_compra_almuerzo').first().click()
   
-  await page.locator('[class="filter-option pull-left"]').click()
-  await page.locator('[rel="1"]').click()
+  // await page.locator('[class="filter-option pull-left"]').click()
+  // await page.locator('[rel="1"]').click()
 
-  await page.locator('#submit-btn').click()
+  // await page.locator('#submit-btn').click()
 }
