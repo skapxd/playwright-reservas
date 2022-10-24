d67a6dac8c09b918c04121e3a99cf41eb2d03604
mLondono <mlondono@q10.com>
2022-10-24 11:38:43 -0500

    build

--- index.js
+++ index.js
@@ -2,39 +2,59 @@
 // Documentacion del paquete -> https://playwright.dev/docs/api/class-playwright
 // Video de ayuda para playWright -> https://www.youtube.com/watch?v=YjbRkt8cew8
 
-const { schedule } = require('node-cron');
-const { chromium } = require('playwright');
-const { sendMail } = require('./mail.js');
-const { nuevoDia } = require('./src/nuevoDia.js');
-const { reservarDia } = require('./src/reservarDia.js');
+const { schedule } = require("node-cron");
+const { chromium } = require("playwright");
+const { sendMail } = require("./mail.js");
+const { nuevoDia } = require("./src/nuevoDia.js");
+const { reservarDia } = require("./src/reservarDia.js");
 
-async function main(){
-  const browser = await chromium.launch({headless: true})
-  const page = await browser.newPage()
-  
-  await page.goto('https://jack.q10.com/Q10/Asistencia')
+async function main() {
+  const browser = await chromium.launch({ headless: true });
+  const page = await browser.newPage();
 
-  const user = await page.locator('#NombreUsuario').type('jrivera@q10.com')
-  const pass = await page.locator('#Contrasena').type('zxasqw')
+  await page.goto("https://jack.q10.com/Q10/Asistencia");
 
-  await page.click('#submit-btn')
-  
-  const btnRol = page.locator('[data-id="3"]')
-  await btnRol.waitFor()
-  btnRol.click()
+  const user = await page.locator("#NombreUsuario").type("jrivera@q10.com");
+  const pass = await page.locator("#Contrasena").type("zxasqw");
 
-  const reservaDia = await nuevoDia(page)
-  if (reservaDia === false) return await browser.close()
+  await page.click("#submit-btn");
 
-  await reservarDia(page)
+  const btnRol = page.locator('[data-id="3"]');
+  await btnRol.waitFor();
+  btnRol.click();
+
+  const reservaDia = await nuevoDia(page);
+  if (reservaDia === false) return await browser.close();
+
+  await reservarDia(page);
 
   sendMail({
-    email: 'jrivera@q10.com',
-    message: 'Su reserva se ha realizado exitosamente',
-    subject: 'Reserva automatica en JACK'
-  })
+    email: "jrivera@q10.com",
+    message: "Su reserva se ha realizado exitosamente",
+    subject: "Reserva automatica en JACK",
+  });
 
-  return await browser.close()
+  return await browser.close();
 }
 
-schedule('* * 12 * * *',  main)
+schedule("* * 1 * * *", () => {
+
+});
+(function init(params) {
+  // create Date object for current location
+  const date = new Date();
+
+  // convert to milliseconds, add local time zone offset and get UTC time in milliseconds
+  const utcTime = date.getTime() + date.getTimezoneOffset() * 60000;
+
+  // time offset for New Zealand is +12
+  const timeOffset = -5;
+
+  // create new Date object for a different timezone using supplied its GMT offset.
+  const colombiaHours = new Date(utcTime + 3600000 * timeOffset);
+
+  const canReserve = colombiaHours.getHours() === 11 
+  if (!canReserve) return;
+
+  main();
+})();
